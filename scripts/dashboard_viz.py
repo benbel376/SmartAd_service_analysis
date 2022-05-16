0432cead5252c20238e523fa2049e4f8187cf7f8
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-05-16 03:58:05 -0700

    added pie chart plotter

--- scripts/dashboard_viz.py
+++ scripts/dashboard_viz.py
@@ -57,23 +57,55 @@ class VizManager:
         image = Image.open(self.charts_dir+"/"+chart_name)
         st.image(image, caption = desc)
 
+    def save_charts(self, pltc, name, desc):
+        """
+        saves the charts in charts folder and 
+        """
+        self.load_tracker()
 
-    def plot_bar(self, df, title, save = False, name = None, desc = None):
+        pltc.savefig(self.charts_dir+"/"+name)
+        self.chart_tracker_df.append
+        row = {"chart_name":name, "chart_desc":desc}
+        self.chart_tracker_df = self.chart_tracker_df[self.chart_tracker_df['chart_name'] != name]
+        self.chart_tracker_df = self.chart_tracker_df.append(row, ignore_index = True)
+
+        self.save_tracker()
+
+
+    def plot_bar(self, df, col, title, save = False, name = None, desc = None):
         
-        plt.bar(df.iloc[:,1],df.iloc[:,2])
+        plt.bar(df[col[0]],df[col[1]])
         plt.title(title)
-        plt.xlabel(df.columns.to_list()[0])
-        plt.ylabel(df.columns.to_list()[1])
+        plt.xlabel(col[0])
+        plt.ylabel(col[1])
         plt.show()
         if (save):
-            self.load_tracker()
+            self.save_charts(plt, name, desc)
 
-            plt.savefig(self.charts_dir+"/"+name)
-            self.chart_tracker_df.append
-            row = {"chart_name":name, "chart_desc":desc}
-            self.chart_tracker_df = self.chart_tracker_df.append(row, ignore_index = True)
+    
+    def plot_pie(self, df, col, title, save=False, name = None, desc = None):
+        """
+        pie chart plotting function.
+        """
+        # Wedge properties
+        wp = { 'linewidth' : 1, 'edgecolor' : "black" }
 
-            self.save_tracker()
+        # Creating autocpt arguments
+        def func(pct, allvalues):
+            absolute = int(pct / 100.*np.sum(allvalues))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        
+        fig, ax = plt.subplots(figsize =(10, 7))
+        wedges, texts, autotexts = ax.pie(df[col[1]],
+                                    autopct = lambda pct: func(pct, df[col[1]]),
+                                    labels = df[col[0]].to_list(),
+                                    startangle = 90,
+                                    wedgeprops = wp,)
+
+        plt.setp(autotexts, size = 8, weight ="bold")
+        ax.set_title(title)
+        if(save):
+            self.save_charts(plt, name, desc)
 
     def plot_scatter(self):
         pass
