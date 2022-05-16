082e9d8f69058125de2ea5a75605679ce9e4464a
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-05-16 01:44:28 -0700

    set up dashboard manager base resources

---
+++ scripts/dashboard_viz.py
@@ -0,0 +1,24 @@
+import os
+
+class VizManager:
+    """
+    a class that manages the extraction of charts from notebooks
+    and deploying them on streamlit app.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    
+    def extract_chart(image):
+        """
+        saving images and their descriptions in charts folder 
+        and a tracking csv file.
+        """
+
+    
+    def load_charts():
+        """
+        loading charts from charts folder and descriptions from
+        the tracking csv file.    
+        """
